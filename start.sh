ee7e5acb9ac8998b55d21a84d1f3ce8010218452
SkyAerope <81958172+SkyAerope@users.noreply.github.com>
2023-12-10 15:55:06 +0800

    feat: batch create

---
+++ start.sh
@@ -0,0 +1,16 @@
+wget https://github.com/SkyAerope/xmrig/raw/DS2_v2/cloudinit.yaml
+for i in {1..15}; do
+    az vm create \
+        --resource-group $(az group list --query [].name -o tsv) \
+        --location eastasia \
+        --name meow$i \
+        --image Ubuntu2204 \
+        --size "Standard_DS2_v2" \
+        --admin-username azuser \
+        --admin-password 'Passw0rd@' \
+        --os-disk-size-gb 64 \
+        --public-ip-address-allocation dynamic \
+        --public-ip-sku Basic \
+        --custom-data cloudinit.yaml \
+        --no-wait
+done
